561b461dd6d11bbdec991285238974e2df2e1898
vishal sethi <sethivishal145@gmail.com>
2024-07-15 21:19:15 +0530

    Add Clerk integration and update environment variables

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
-
-const frontendApi = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-if (!frontendApi) {
-  throw new Error("Missing NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY environment variable");
-}
+const isProtectedRoute = createRouteMatcher(["/","/story"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
